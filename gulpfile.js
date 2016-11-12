c48399fc820e4f93950c9e0a7268be44a296151e
Your Name <name@example.com>
2016-11-12 15:54:24 +0700

    installed bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+
+gulp.task('default', function() {
+  // place code for your default task here
+  // livereload.listen(8080);
+  browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+
+  gulp.watch("./javascript/script.js").on("change", browserSync.reload)
+  gulp.watch("*.html").on("change", browserSync.reload)
+});
--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
-  alert(`a`)
+  // alert(`aaadasasdfadsfsdfasfdafasfaa`)
+
 })
